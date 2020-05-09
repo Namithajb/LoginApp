581e9ff263257f41b2c46bde7136bafaee933561
Namithajb <32310984+Namithajb@users.noreply.github.com>
2020-05-09 18:46:02 +0530

    Create schema.sql
    Adding schema

---
+++ schema/schema.sql
@@ -0,0 +1,35 @@
+
+DROP DATABASE IF EXISTS `user`;
+CREATE DATABASE user;
+use user;
+DROP TABLE IF EXISTS `invoice`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice` (
+  `invoice_id` int NOT NULL AUTO_INCREMENT,
+  `p_price` float NOT NULL,
+  `vat` float NOT NULL,
+  `no_of_part` float NOT NULL,
+  `total_price` float NOT NULL,
+  `total_vat` float NOT NULL,
+  `total_amt` float NOT NULL,
+  PRIMARY KEY (`invoice_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
