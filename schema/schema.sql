f3000f03abf4d42563567f20101c2315062387ef
Namithajb <32310984+Namithajb@users.noreply.github.com>
2020-05-23 23:55:11 +0530

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -14,21 +14,30 @@ CREATE TABLE `invoice` (
   `total_vat` float NOT NULL,
   `total_amt` float NOT NULL,
   PRIMARY KEY (`invoice_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `username` varchar(45) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `address` varchar(45) DEFAULT NULL,
   `phone` varchar(15) DEFAULT NULL,
-  `role` varchar(45) DEFAULT 'Admin',
   `userid` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`userid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=0;
+
+DROP TABLE IF EXISTS `authority`;
+CREATE TABLE `authority` (
+  `userid` int NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username_UNIQUE` (`userid`),
+  CONSTRAINT `FK_Userid` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
